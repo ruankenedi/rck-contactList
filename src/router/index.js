481112ccdb7d791baf7224fdcfcbba8d806e5374
ruankenedi <ruankenedi01@gmail.com>
2018-07-24 10:29:50 -0300

    First commit

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+function load(component) {
+	return () => System.import(`../views/${component}`);
+}
+
+Vue.use(VueRouter);
+
+export default Router;
+
+function Router(route) {
+	const router = new VueRouter({
+		routes: [{
+			name: 'home',
+			path: '/',
+			component: load('Index'),
+			// redirect: ,
+			meta: { requiresAuth: true },
+			children: [
+				{
+					name: 'login',
+					path: '/login',
+					component: load('Login'),
+					meta: { requiresAuth: true }
+				}
+			]
+
+		}]
+	});
+
+	return router;
+}
